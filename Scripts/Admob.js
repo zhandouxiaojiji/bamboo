889ec332c33472e19f4251fdb22b8b76b440f1ac
wong@Mac <zhandouxiaojiji@gmail.com>
2018-11-11 09:31:28 +0800

    add Admob

---
+++ Scripts/Admob.js
@@ -0,0 +1,18 @@
+cc.Class({
+    onLoad: function () {
+        //Add this line to onLoad
+        this.admobInit();
+    },
+
+    admobInit: function() {
+        //finish it after import admob, let it empty for now
+    },
+
+    cacheInterstitial: function() {
+        //finish it after import admob, let it empty for now
+    },
+
+    showInterstitial: function() {
+        //finish it after import admob, let it empty for now
+    },
+}
