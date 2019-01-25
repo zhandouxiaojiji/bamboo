468ecb78d59e526b2953496b48ce74538c6df1a2
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-01-25 17:18:47 +0800

    添加多语言支持

--- bb.js
+++ bb.js
@@ -1,3 +1,8 @@
 var bb = {
+    initLanguage: function(){
+        console.log("bb.initLanguage", cc.sys.languageCode);
+        const i18n = require('LanguageData');
+        i18n.init(cc.sys.languageCode);
+    }
 };
 module.exports = bb;
