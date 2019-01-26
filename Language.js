65c3caf8617c3a9de8b0c2ecf2864350f76666e1
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-01-26 10:59:44 +0800

    区码自动转换

---
+++ Language.js
@@ -0,0 +1,35 @@
+var bb = require("bb");
+var Language = {
+    init: function(){
+        console.log("bb.initLanguage", cc.sys.languageCode, require(cc.sys.languageCode)==undefined);
+        const i18n = require('LanguageData');
+        var code = cc.sys.languageCode;
+        //至少需要提供en一种
+        if(!this.exist(code)){
+            if(code.indexOf('tw') >= 0){
+                if(this.exist('zh-tw')){
+                    code = 'zh-tw';
+                }else {
+                    code = 'en';
+                }
+            }
+            else if(code.indexOf('zh') >= 0){
+                if(this.exist('zh-cn')){
+                    code = 'zh-cn';
+                }else if(this.exist('zh')){
+                    code = 'zh';
+                }else{
+                    code = 'en';
+                }
+            
+            }else{
+                code = 'en';
+            }
+        }
+        i18n.init(code);
+    },
+    exist: function(code){
+        return window.i18n.languages[code]
+    }
+}
+bb.Language = Language;
--- bb.js
+++ bb.js
@@ -1,12 +1,3 @@
 var bb = {
-    initLanguage: function(){
-        console.log("bb.initLanguage", cc.sys.languageCode);
-        const i18n = require('LanguageData');
-        var code = cc.sys.languageCode;
-        if(code == 'zh'){ //安卓暂时不支持区码
-            code = 'zh-cn';
-        }
-        i18n.init(code);
-    }
 };
 module.exports = bb;
