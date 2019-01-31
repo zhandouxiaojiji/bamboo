09c50d17a9ca3ff5011c38d74c2a8fd657982ea3
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-01-31 12:13:39 +0800

    log自动滚动

--- Console/ConsoleService.js
+++ Console/ConsoleService.js
@@ -1,9 +1,16 @@
 var bb = require("bb");
 var Console = {
     event: {
-        UPDATE_LOG: "UPDATE_LOG"
+        UPDATE_LOG: "UPDATE_LOG",
+        UPDATE_CUSTOM: "UPDATE_CUSTOM"
     },
     log: "",
+    customs: [],
+
+    init: function(){
+
+    },
+
     addLog: function(str){
         this.log = this.log + str + "\n";
         bb.GlobalEvent.dispatch(this.event.UPDATE_LOG, this.log);
@@ -11,6 +18,25 @@ var Console = {
     clearLog: function(){
         log = "";
         bb.GlobalEvent.dispatch(this.event.UPDATE_LOG, this.log);
+    },
+    addCustom(name, callback){
+        this.customs.push({
+            name: name,
+            callback: callback
+        })
+    },
+    doCustom(name){
+        for (const i in this.customs) {
+            if (this.customs.hasOwnProperty(i)) {
+                const element = this.customs[i];
+                element.callback.call(element.callback);
+            }
+        }
+        var callback = this.customs[name];
+        if(!callback){
+            bb.log("custom not exist:", name);
+        }
+        callback.call(callback);
     }
 };
 bb.Console = Console;
--- Console/LogView.js
+++ Console/LogView.js
@@ -30,8 +30,10 @@ cc.Class({
 
     updateLog(str){
         this.label.string = str;
-        this.label._updateRenderData(true);
-        this.content.height = this.label.node.height;
+        this.content.height = this.label.node.height+30;
+        var view_height = this.node.height;
+        var content_height = this.content.height;
+        this.content.y = content_height < view_height ? view_height/2 : content_height - view_height/2;
     }
 
     // update (dt) {},
