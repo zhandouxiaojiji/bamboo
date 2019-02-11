4ac283af3e2cc859619dc35a738de4ad3575b29e
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-02-11 20:10:28 +0800

    test google play

--- SDKBox/SdkboxPlay.js
+++ SDKBox/SdkboxPlay.js
@@ -1,10 +1,39 @@
 var bb = require("bb");
 bb.SdkboxPlay = {
+    event: {
+        CON_STATUS: "CON_STATUS"
+    },
     init(){
+        if(!cc.sys.isMobile){
+            return;
+        }
+        sdkbox.PluginSdkboxPlay.init();
+    },
+    signin(){
         if(!cc.sys.isMobile){
             bb.log("not mobile platform");
             return;
         }
-        sdkbox.PluginSdkboxPlay.init();
+        sdkbox.PluginSdkboxPlay.signin(true);
+        sdkbox.PluginSdkboxPlay.setListener({
+            onConnectionStatusChanged: function(status){
+                bb.log("onConnectionStatusChanged", status);
+                bb.log("isSignin", sdkbox.PluginSdkboxPlay.isSignedIn());
+                bb.log("name", this.getInfo("name"));
+                bb.GlobalEvent.dispatch(this.event.CON_STATUS, status);
+            }
+        })
+    },
+    getInfo(field){
+        if(!cc.sys.isMobile){
+            return;
+        }
+        return sdkbox.PluginSdkboxPlay.getPlayerAccountField(field);
+    },
+    isSignin(){
+        if(!cc.sys.isMobile){
+            return false;
+        }
+        return sdkbox.PluginSdkboxPlay.isSignin();
     }
 };
--- Test/TestSdkboxPlay.js
+++ Test/TestSdkboxPlay.js
@@ -8,15 +8,25 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
+    onConnect(status){
+        bb.log("on connect !!!", status);
+    },
     testUser() {
         bb.Console.addCustom("SdkboxPlay", function(){
             bb.SdkboxPlay.init();
+            bb.SdkboxPlay.signin();
         });
     },
 
+
     start () {
+        bb.GlobalEvent.on(bb.SdkboxPlay.event.CON_STATUS, this.onConnect, this);
         this.testUser();
     },
+
+    onDestroy(){
+        bb.GlobalEvent.off(bb.SdkboxPlay.event.CON_STATUS, this.onConnect, this)
+    }
 
     // update (dt) {},
 });
