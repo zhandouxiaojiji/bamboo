d04029c943588b0f2b5f440b6c57ea0a84e17196
wong <zhandouxiaojiji@gmail.com>
2019-03-19 00:15:24 +0800

    fix

--- bb.js
+++ bb.js
@@ -28,7 +28,7 @@ module.exports = {
 
     getData(k, defaultValue){
         var v = cc.sys.localStorage.getItem(k);
-        if(typeof(v) == "null" || typeof(v) == "undefined" || v == "undefined"){
+        if(!v || typeof(v) == "null" || typeof(v) == "undefined" || v == "undefined"){
             cc.sys.localStorage.setItem(k, defaultValue);
             return defaultValue;
         }
