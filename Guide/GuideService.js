d3fb61c9bfba872ba1fb24f5dc47ba35e63edbfa
wong <zhandouxiaojiji@gmail.com>
2019-06-15 16:29:42 +0800

    change GuideService

--- Guide/GuideService.js
+++ Guide/GuideService.js
@@ -1,23 +1,23 @@
 var bb = require("bb");
 bb.Guide = {
     EventType: {
-
+        SHOW: "SHOW",
     },
     guides: {},
-    init(prop) {
-        this.prop = prop;
-        this.guides = {};
-        for(let k in prop){
-            let v = prop[k];
-            let guide = this.guides[v.TYPE] || [];
-            guide.push(v);
-            this.guides[v.TYPE] = guide;
+    init(data) {
+        for (let type in data) {
+            self.guides[type] = data[type];
         }
-        for(let k in this.guides){
-            let v = this.guides[k];
-            v.sort((a, b) => {
-                return a.ID - b.ID
-            })
+    },
+
+    show(type) {
+        if (this.guides[type]) {
+            return;
         }
+        bb.dispatch(this.EventType.SHOW, type);
+    },
+
+    fini(type) {
+        this.guides[type] = true
     },
 };
