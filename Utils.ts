bef7a5aa9fd79e34b4999611e066b12627adfd9e
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2020-01-20 19:59:25 +0800

    add Utils

---
+++ Utils.ts
@@ -0,0 +1,8 @@
+//生成从minNum到maxNum的随机数
+export function randomNum(minNum: number, maxNum?: number) {
+  if (maxNum !== undefined) {
+    return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+  } else {
+    Math.floor(Math.random() * (minNum + 1));
+  }
+} 
